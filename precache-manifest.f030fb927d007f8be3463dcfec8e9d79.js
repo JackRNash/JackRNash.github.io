8fe3e0ea5dbfb4679cc22b1e4d4a735a2f11e64f
Jack Nash <jrn79@cornell.edu>
2022-03-30 19:31:20 -0400

    Updates

---
+++ precache-manifest.f030fb927d007f8be3463dcfec8e9d79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df7490797533513f9f6a033aaaa2d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22a15725bdca99f1d8db",
+    "url": "/static/css/2.6f4ee0a7.chunk.css"
+  },
+  {
+    "revision": "db180786ad1d9095436a",
+    "url": "/static/css/main.e15a87a7.chunk.css"
+  },
+  {
+    "revision": "22a15725bdca99f1d8db",
+    "url": "/static/js/2.5fe59b1a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.5fe59b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db180786ad1d9095436a",
+    "url": "/static/js/main.32c274bd.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
